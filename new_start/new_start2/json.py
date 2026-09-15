9675cd49ff3e8939480fe4a4013a9c43aaf1ae25
Denis <testdenis18@gmail.com>
2026-09-15 23:11:19 +0300

    json

--- new_start/new_start2/json.py
+++ new_start/new_start2/json.py
@@ -1,3 +1,8 @@
-user_book_name = str(input("Book name:"))
-user_book_author = str(input("Book author:"))
-user_book_year = str(input("Book author:"))
+import json
+while True:
+    user_book_name = str(input("Book name:"))
+    user_book_author = str(input("Book author:"))
+    user_book_year = str(input("Book year:"))
+    data ={"name":"Jong","book_author":"master","book_year":"123"}
+    with open("books.json","w",encoding="utf-8") as file:
+        json.dump(data)
